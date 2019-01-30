5b0753b33ceeb89cfba23eb675d4a06d0587b6b9
Adrienne Yao <adrienne.yao@icloud.com>
2019-01-30 06:18:00 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -3,6 +3,6 @@ INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy"
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 6, "female", "brown", "calm", 1);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 2, "male", "brown", "calm", 1);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 13, "female", "brown", "calm", 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 2, "male", "brown", "calm", 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 6, "female", "brown", "calm", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 2, "male", "dark brown", "calm", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 6, "female", "dark brown", "calm", 1);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 2, "male", "brown", "calm", 1);
