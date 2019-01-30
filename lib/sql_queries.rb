f29590f6c66b569c798ecda1de824e9502c67a87
Adrienne Yao <adrienne.yao@icloud.com>
2019-01-30 06:25:47 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -1,7 +1,7 @@
 require "pry"
 
 def selects_all_female_bears_return_name_and_age
-  "SELECT name, age FROM bears WHERE gender = \"female\";"
+  "SELECT name, age FROM bears WHERE gender = \"F\";"
 end
 
 def selects_all_bears_names_and_orders_in_alphabetical_order
