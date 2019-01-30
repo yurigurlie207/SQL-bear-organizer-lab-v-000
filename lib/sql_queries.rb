453c80a622817e76fe9f52d9cd88c2705aaac453
Adrienne Yao <adrienne.yao@icloud.com>
2019-01-30 06:17:22 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -21,7 +21,7 @@ def select_youngest_bear_and_returns_name_and_age
 end
 
 def selects_most_prominent_color_and_returns_with_count
-  "Write your SQL query here"
+  "SELECT count(color) as color_count GROUP BY color ORDER BY color_count DESC limit 1;"
 end
 
 def counts_number_of_bears_with_goofy_temperaments
